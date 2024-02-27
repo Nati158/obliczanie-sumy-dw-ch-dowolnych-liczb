aaaa88db1974f33fa982871b56cd20e663dd3e33
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-02-27 21:46:59 +0100

    Add files via upload

---
+++ main34.py
@@ -0,0 +1,8 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    sum_result = add_numbers(num1, num2)
+    print("The sum of", num1, "and", num2, "is:", sum_result)
